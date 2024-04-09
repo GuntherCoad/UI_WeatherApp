de5f4157c8b264f0e7626136cf7f7a757438c224
GuntherCoad <gunthercoad@yahoo.com>
2024-04-09 10:43:31 -0500

    Mainly worked on search box css and placeholder values.

--- static/js/content.js
+++ static/js/content.js
@@ -1,7 +1,7 @@
 const apiKEY = "27eedd9e3f839130d3c83e4b0ed69202";
 const apiENDPOINT = "api.openweathermap.org"
-const weatherResultTemplate = document.querySelector("[search-result-template]");
-const weatherResultContainer = document.getElementById("results");
+const weatherResultTemplate = document.getElementById("search-result-template");
+
 
 /**
  * @description takes parameter limit. This displays "limit" number of locations related to the value in the search box. 
@@ -11,7 +11,6 @@ const weatherResultContainer = document.getElementById("results");
  */
 function getWeather(limit)
 {
-    
     const search = document.getElementById("query");
     const val = search.value;
     
@@ -19,11 +18,9 @@ function getWeather(limit)
     .then(res => res.json())
     .then(data => {
         data.forEach(element => {
-            //const card = weatherResultTemplate.content.cloneNode(true).children;
-            //console.log(element)
-            const markup = `<a href="/temp?q=${element.name}">${element.name}</a><br>`;
+            var card = weatherResultTemplate.content.cloneNode(true).children;
+            card.getElementById("data-header").innerHTML = element.name;
             
-            document.getElementById("results").insertAdjacentHTML('beforeend', markup)
         });
             
         
