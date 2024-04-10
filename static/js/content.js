2b42e408c430c32fdcf83875b1e7ee38ca8e6bb1
GuntherCoad <gunthercoad@yahoo.com>
2024-04-10 14:53:54 -0500

    Results page placeholder objects made, shows on search submit

--- static/js/content.js
+++ static/js/content.js
@@ -1,8 +1,9 @@
 const apiKEY = "27eedd9e3f839130d3c83e4b0ed69202";
 const apiENDPOINT = "api.openweathermap.org"
-const weatherResultTemplate = document.getElementById("search-result-template");
+
 const query = document.getElementById("query");
 const resultbox = document.getElementById("results");
+const searchButton = document.getElementById("querySearch");
 
 /**
  * @description takes parameter limit. This displays "limit" number of locations related to the value in the search box. 
@@ -10,7 +11,7 @@ const resultbox = document.getElementById("results");
  * 
  * @param {*} limit the number of related searches that populate upon typing
  */
-function getWeather(limit)
+function getSearchResults(limit)
 {
     const search = document.getElementById("query");
     const val = search.value;
@@ -25,6 +26,11 @@ function getWeather(limit)
     .catch(error => console.error(error));
 }
 
+/**
+ * @description displays the search results of the getSearchResults() method. result is turned into an array that is filtered based on country(US).
+ * 
+ * @param {*} result The json list of relevant search items from the api response.
+ */
 function displayResult(result) {
     const content = result.flatMap((list)=> {
         if(list.country.match(/US/))            //matching US country code
@@ -43,6 +49,25 @@ function displayResult(result) {
         resultbox.innerHTML = "";
 }
 
+function onSearch() {
+    const homeElem = document.getElementsByClassName("home");
+    const resultElem = document.getElementsByClassName("result-weather");
+    for (const element of homeElem) {
+        element.setAttribute("style", "display: none");
+    }
+    for(const element of resultElem) {
+        element.setAttribute("style", "display:block");
+    }
+
+        console.log(resultElem);
+        console.log(homeElem);
+    }
+    
+
 query.addEventListener('keyup', function () {
-    getWeather(5);
+    getSearchResults(5);
 }, 'false');
+
+searchButton.addEventListener('click', function() {
+    onSearch();
+}, "false");
