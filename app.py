79c0afcc6e6e41ea0342005490827d49927e8a76
daniel <whadmia@gmail.com>
2024-04-13 19:02:16 -0500

    Update app.py to its own function.

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 from flask import Flask, render_template
-app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return render_template("index.html")
+def create_app(test_config=None):
 
-@app.route("/temp")
-def temp():
-    return render_template("tempPage.html")
+    app = Flask(__name__)
+
+
+    @app.route("/")
+    def home():
+        return render_template("index.html")
+    
+    @app.route("/temp")
+    def temp():
+        return render_template("tempPage.html")
+
+    return app
