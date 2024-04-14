66432c5056cf900dad27219b8f76cb652b0fba06
daniel <whadmia@gmail.com>
2024-04-13 22:46:04 -0500

    Initial location.py to get current user's location.

---
+++ location.py
@@ -0,0 +1,9 @@
+import requests
+from config import GEO_SERVICE
+
+# Not paying for an API so state is indeterminant.
+def get_current_location():
+    """Returns current location as a list[latitude,longitude]"""
+    r = requests.get(GEO_SERVICE)
+    cur_loc = r.text.split()
+    return cur_loc
