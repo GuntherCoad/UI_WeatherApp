803803e38840de69a5869dc6693450f4c887b414
daniel <whadmia@gmail.com>
2024-04-15 20:08:42 -0500

    Add animated icons based on weather code.

--- static/js/today.js
+++ static/js/today.js
@@ -16,4 +16,38 @@ document.addEventListener('DOMContentLoaded', function() {
     } else {
         imgEl.src = '/static/images/rain.png';
     }
+
+    switch (wcode) {
+        case 1:
+        case 2:
+        case 3:
+            imgEl.src = '/static/images/fill/darksky/clear-day.svg';
+            break;
+        case 45:
+        case 48:
+            imgEl.src = '/static/images/fill/darksky/fog.svg';
+            break;
+        case 51:
+        case 53:
+        case 55:
+        case 56:
+        case 57:
+            imgEl.src = '/static/images/fill/darksky/drizzle.svg';
+            break;
+        case 61:
+        case 63:
+        case 65:
+        case 66:
+        case 67:
+            imgEl.src = '/static/images/fill/darksky/rain.svg';
+            break;
+        case 71:
+        case 73:
+        case 75:
+        case 77:
+            imgEl.src = '/static/images/fill/darksky/snow.svg';
+            break;
+        default:
+            imgEl.src = '/static/images/fill/darksky/clear-day.svg';
+    }
 });
